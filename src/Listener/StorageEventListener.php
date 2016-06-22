dfb239e06ab1d9609783a5f755686e483b42ddda
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-06-22 13:36:20 +0200

    sending storage events via pusher

--- src/Listener/StorageEventListener.php
+++ src/Listener/StorageEventListener.php
@@ -4,6 +4,8 @@ namespace Bolt\Extension\Ohlandt\PusherRealtime\Listener;
 
 use Bolt\Events\StorageEvent;
 use Bolt\Events\StorageEvents;
+use Bolt\Extension\Ohlandt\PusherRealtime\Event\PusherEvents;
+use Bolt\Extension\Ohlandt\PusherRealtime\Event\PusherStorageEvent;
 use Pimple as Container;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -27,18 +29,40 @@ class StorageEventListener implements EventSubscriberInterface
         $this->config = $config;
     }
 
-     /**
-      * Handles POST_SAVE storage event
-      *
-      * @param StorageEvent $event
-      */
-     public function onPostSave(StorageEvent $event)
-     {
-         $id = $event->getId();
-         $contenttype = $event->getContentType();
-         $record = $event->getContent();
-         $created = $event->isCreate();
-     }
+    /**
+     * Handles POST_SAVE storage event
+     *
+     * @param StorageEvent $event
+     */
+    public function onPostSave(StorageEvent $event)
+    {
+        $id = $event->getId();
+        $contenttype = $event->getContentType();
+        $record = $event->getContent();
+        $created = $event->isCreate();
+
+        $storageEvent = new PusherStorageEvent($id, $contenttype, $record);
+        $this->container['dispatcher']->dispatch(PusherEvents::SET_STORAGE_EVENT_NAMES, $storageEvent);
+
+        $config = $this->container['pusher.config'];
+        $data = [
+            'id' => $id,
+            'contenttype' => $contenttype,
+            'record' => $record
+        ];
+
+        if ($config->isValid() && $contenttype = $config->getContentType($contenttype)) {
+            if ($created) {
+                if ($contenttype->get('created')) {
+                    $this->container['pusher']->trigger($storageEvent->getChannelName(), $storageEvent->getCreatedEventName(), $data);
+                }
+            } else {
+                if ($contenttype->get('updated')) {
+                    $this->container['pusher']->trigger($storageEvent->getChannelName(), $storageEvent->getUpdatedEventName(), $data);
+                }
+            }
+        }
+    }
 
     /**
      * Handles POST_DELETE storage event
@@ -50,6 +74,22 @@ class StorageEventListener implements EventSubscriberInterface
         $id = $event->getId();
         $contenttype = $event->getContentType();
         $record = $event->getContent();
+
+        $storageEvent = new PusherStorageEvent($id, $contenttype, $record);
+        $this->container['dispatcher']->dispatch(PusherEvents::SET_STORAGE_EVENT_NAMES, $storageEvent);
+
+        $config = $this->container['pusher.config'];
+        $data = [
+            'id' => $id,
+            'contenttype' => $contenttype,
+            'record' => $record
+        ];
+
+        if ($config->isValid() && $contenttype = $config->getContentType($contenttype)) {
+            if ($contenttype->get('deleted')) {
+                $this->container['pusher']->trigger($storageEvent->getChannelName(), $storageEvent->getDeletedEventName(), $data);
+            }
+        }
     }
 
     /**
